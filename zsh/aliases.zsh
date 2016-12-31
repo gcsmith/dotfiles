8fe2a452d73b1b05efd36dbbe3490ce0e1596c5c
gcsmith <smith.garrett.c@gmail.com>
2016-12-31 17:38:55 -0500

    use gnu coreutils in macos

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,15 @@
+uname_value=`uname`
+
 alias grep='grep --colour=auto'
-alias ls='ls --color=auto -G'
-alias ll='ls -l --group-directories-first'
-alias la='ls -Al'
 alias tmux='TERM=screen-256color tmux'
+
+if [[ "$uname_value" == 'Darwin' ]]; then
+    alias ls='gls --color=auto -G'
+    alias ll='gls --color=auto -l --group-directories-first'
+    alias la='gls --color=auto -Al'
+else
+    alias ls='ls --color=auto -G'
+    alias ll='ls --color=auto -l --group-directories-first'
+    alias la='ls --color=auto -Al'
+fi
 
