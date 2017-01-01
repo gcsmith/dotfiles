dd19da44e283f1b713dc520e73b4be9fd39d81bd
gcsmith <smith.garrett.c@gmail.com>
2017-01-01 13:57:16 -0500

    fix la alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,15 +1,13 @@
-uname_value=`uname`
-
 alias grep='grep --colour=auto'
 alias tmux='TERM=screen-256color tmux'
 
+uname_value=`uname`
 if [[ "$uname_value" == 'Darwin' ]]; then
     alias ls='gls --color=auto -G'
-    alias ll='gls --color=auto -l --group-directories-first'
-    alias la='gls --color=auto -Al'
 else
     alias ls='ls --color=auto -G'
-    alias ll='ls --color=auto -l --group-directories-first'
-    alias la='ls --color=auto -Al'
 fi
+
+alias ll='ls --group-directories-first -l'
+alias la='ll -A'
 
